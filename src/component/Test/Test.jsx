f3667f6597331fd8edbf49aae0ae30af481ad66a
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-11-02 09:47:06 +0200

    JSX Syntax Release #1

--- src/component/Test/Test.jsx
+++ src/component/Test/Test.jsx
@@ -1,12 +1,21 @@
 import "./Test.css";
+import React from "react";
 
 const Test = () => {
+  /*
   return (
     <>
       <div className="App">
         <h1>Tis is Test File</h1>
       </div>
     </>
+  );
+  */
+
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement("h1", null, "This is test file")
   );
 };
 
