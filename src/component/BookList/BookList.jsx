fa4898af3f16efc2d7946449183b6a297255003f
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-11-02 19:08:14 +0200

    Changing State with JS event Release #1

--- src/component/BookList/BookList.jsx
+++ src/component/BookList/BookList.jsx
@@ -3,28 +3,39 @@ import { Component } from "react";
 import { Book } from "../index";
 
 export default class BookList extends Component {
-  #state = {
+  state = {
     books: [
       { BookName: "Think and Grow Rich", Writer: "George" },
       { BookName: "Rich Dad and poor Dad", Writer: "Dan Brown" },
       { BookName: "The Alchemist Riyad MS", Writer: "Paulo" },
     ],
   };
+
+  changeBookState = () => {
+    this.setState({
+      books: [
+        { BookName: "Think and Grow Rich NEW", Writer: "George NEW" },
+        { BookName: "Rich Dad and poor Dad NEW", Writer: "Dan Brown NEW" },
+        { BookName: "The Alchemist Riyad MS NEW", Writer: "Paulo NEW" },
+      ],
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>Tihs is BookList </h1>
+        <button onClick={this.changeBookState}>Change State</button>
         <Book
-          BookName={this.#state.books[0].BookName}
-          Writer={this.#state.books[0].Writer}
+          BookName={this.state.books[0].BookName}
+          Writer={this.state.books[0].Writer}
         />
         <Book
-          BookName={this.#state.books[1].BookName}
-          Writer={this.#state.books[1].Writer}
+          BookName={this.state.books[1].BookName}
+          Writer={this.state.books[1].Writer}
         />
         <Book
-          BookName={this.#state.books[2].BookName}
-          Writer={this.#state.books[2].Writer}
+          BookName={this.state.books[2].BookName}
+          Writer={this.state.books[2].Writer}
         />
       </div>
     );
